1eead9346ead5de3498dc62f2894e234cf8ebc72
leowi <jhoshep_1999@hotmail.com>
2020-10-14 22:28:32 -0500

    app terminada

--- app/src/main/java/com/example/indp_lab04/AddActivity.java
+++ app/src/main/java/com/example/indp_lab04/AddActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import android.widget.ListView;
 import java.util.ArrayList;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+
 public class AddActivity extends Activity {
     EditText editText1;
     EditText editText2;
@@ -18,18 +20,38 @@ public class AddActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        editText1=(EditText)findViewById(R.id.editText1);
-        editText1=(EditText)findViewById(R.id.editText2);
-        editText1=(EditText)findViewById(R.id.editText3);
+        editText1=(EditText)findViewById(R.id.editTextNumber2);
+        editText2=(EditText)findViewById(R.id.editText2);
+        editText3=(EditText)findViewById(R.id.editText3);
         button1=(Button)findViewById(R.id.button2);
         button2=(Button)findViewById(R.id.button3);
         button1.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                String message=editText1.getText().toString();
+
+
+                String message1=editText1.getText().toString();
+                String message2=editText2.getText().toString();
+                String message3=editText3.getText().toString();
+                if(message1.equals("")||message2.equals("") || message3.equals("")) {
+                    Toast.makeText(getBaseContext(),"Rellene los datos",Toast.LENGTH_SHORT).show();
+                }else {
+                    int message1int=Integer.parseInt(message1);
+                    Intent intent = new Intent();
+                    intent.putExtra("CUI", message1int);
+                    intent.putExtra("APELLIDOS", message2);
+                    intent.putExtra("NOMBRES", message3);
+
+                    setResult(2, intent);
+                    finish();//finishing activity
+                }
+            }
+        });
+        button2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
                 Intent intent=new Intent();
-                intent.putExtra("MESSAGE",message);
-                setResult(2,intent);
+                setResult(3,intent);
                 finish();//finishing activity
             }
         });
